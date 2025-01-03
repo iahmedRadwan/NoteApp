6dea323b5da96eaec979116b16dc6c7fb9758241
iahmedRadwan <ahmedradwanflutter@gmail.com>
2025-01-03 02:18:28 +0100

    create an add_note cubit and it's states

---
+++ lib/features/notes/view_models/add_note_cubit/add_note_state.dart
@@ -0,0 +1,12 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+sealed class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoadingState extends AddNoteState {}
+
+final class AddNoteErrorState extends AddNoteState {}
+
+final class AddNoteSuccessState extends AddNoteState {}
